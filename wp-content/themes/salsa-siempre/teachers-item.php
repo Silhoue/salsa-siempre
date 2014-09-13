cc46f325a8f16fa02fb3e8a3eb09b8917eec7f3f
Silhoue <Silhoue@users.noreply.github.com>
2014-09-14 01:10:13 +0200

    Style teachers page

---
+++ wp-content/themes/salsa-siempre/teachers-item.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package Salsa Siempre
+ */
+?>
+
+--><section class="news-item">
+	<a href="<?php esc_url(the_permalink()); ?>" rel="bookmark">
+		<?php if ( get_the_post_thumbnail() ): ?>
+		<div class="news-item-image-wrapper">
+			<?php the_post_thumbnail("teachers-item-image", array("class" => "news-item-image", "alt" => "")); ?>
+		</div>
+		<?php endif; ?>
+		<div class="teachers-item-caption">
+			<h2 class="teachers-item-title"><?php the_title(); ?></h2>
+		</div>
+	</a>
+</section><!--
--- wp-content/themes/salsa-siempre/teachers.php
+++ wp-content/themes/salsa-siempre/teachers.php
@@ -1,16 +1,24 @@
 <?php
 /**
+ * Template Name: Teachers
  * @package Salsa Siempre
  */
 ?>
-
 <?php get_header(); ?>
 
 <main role="main">
+	<h1 class="page-title">Instruktorzy</h1>
+	<?php
+		$teachers = array(
+			'post_type' => 'teacher',
+			'order' => 'ASC'
+		);
+		query_posts($teachers);
+	?>
 	<?php if ( have_posts() ) :
 		echo "<!--";
 		while ( have_posts() ) : the_post();
-			get_template_part("news-item");
+			get_template_part("teachers-item");
 		endwhile;
 		echo "-->";
 	else :
