8bafd2331f9b55042fd6904f5715dc301d2b1b40
Silhoue <Silhoue@users.noreply.github.com>
2014-10-14 02:04:30 +0200

    Remove front-page.php, use index.php instead

--- wp-content/themes/salsa-siempre/index.php
+++ wp-content/themes/salsa-siempre/index.php
@@ -10,36 +10,20 @@
  *
  * @package Salsa Siempre
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php salsa_siempre_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<main role="main">
+	<?php if (have_posts()) {
+		echo "<!--";
+		while (have_posts()) { the_post();
+			get_template_part("news-item");
+		}
+		echo "-->";
+	} else { ?>
+		Brak aktualności
+	<?php } ?>
+</main>
 
 <?php get_footer(); ?>
