8ce7ea074ed4cdd4587d24d5ca4f394bbf43bafa
Silhoue <Silhoue@users.noreply.github.com>
2014-10-17 01:27:38 +0200

    Add levels page

---
+++ wp-content/themes/salsa-siempre/levels.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Levels
+ * @package Salsa Siempre
+ */
+?>
+<?php get_header(); ?>
+
+<main role="main">
+	<?php while (have_posts()) { the_post(); ?>
+		<h1 class="page-title"><?php the_title(); ?></h1>
+	<?php }
+
+	$types = array(
+		'post_type' => 'level'
+	);
+	query_posts($types);
+	while (have_posts()) { the_post();
+		get_template_part("page-content");
+	} ?>
+</main>
+
+<?php get_footer(); ?>
