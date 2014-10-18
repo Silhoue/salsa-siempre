10e6285124c03841abf8437f56576f0413728b58
Silhoue <Silhoue@users.noreply.github.com>
2014-10-18 15:36:05 +0200

    Update special offers page

--- wp-content/themes/salsa-siempre/special-offers.php
+++ wp-content/themes/salsa-siempre/special-offers.php
@@ -7,9 +7,9 @@
 <?php get_header(); ?>
 
 <main role="main">
-	<?php while (have_posts()) { the_post(); ?>
-		<h1 class="page-title"><?php the_title(); ?></h1>
-	<?php }
+	<?php while (have_posts()) { the_post();
+		get_template_part("page-content");
+	}
 
 	$types = array(
 		'post_type' => 'special-offer'
